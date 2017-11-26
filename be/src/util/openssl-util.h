baaa5f92706cb45281d6c1d7795b54d733229199
Xianda Ke <kexianda@gmail.com>
2017-11-26 15:35:22 +0800

    IMPALA-6128: Spill-to-disk Encryption(AES-CFB + SHA256) is slow CFB mode is stream cipher and is secure when use different nonce/IV for every message. However it would be a performance bottleneck. CTR mode is also stream cipher and is secure, 4~6x faster than CFB mode in OpenSSL. AES-CTR+SHA256 is about 40~70% faster than AES-CFB+SHA256.
    CTR mode is used if OpenSSL version>=1.0.1 at runtime, otherwise
    fall back to CFB mode.
    
    Testing:
    run runtime tmp-file-mgr-test, openssl-util-test, buffer-pool-test and buffered-tuple-stream-test
    
    Change-Id: I9debc240615dd8cdbf00ec8730cff62ffef52aff

--- be/src/util/openssl-util.h
+++ be/src/util/openssl-util.h
@@ -19,15 +19,24 @@
 #define IMPALA_UTIL_OPENSSL_UTIL_H
 
 #include <openssl/aes.h>
+#include <openssl/evp.h>
 #include <openssl/sha.h>
 
 #include "common/status.h"
 
 namespace impala {
 
+#define OPENSSL_VERSION_1_0_1 0x1000100L
+
 /// Add entropy from the system RNG to OpenSSL's global RNG. Called at system startup
 /// and again periodically to add new entropy.
 void SeedOpenSSLRNG();
+
+enum AES_CIPHER_MODE {
+  AES_256_CTR,
+  AES_256_CFB,
+  AES_256_GCM // not supported now.
+};
 
 /// The hash of a data buffer used for checking integrity. A SHA256 hash is used
 /// internally.
@@ -47,7 +56,7 @@ class IntegrityHash {
 /// The key and initialization vector (IV) required to encrypt and decrypt a buffer of
 /// data. This should be regenerated for each buffer of data.
 ///
-/// We use AES with a 256-bit key and CFB cipher block mode, which gives us a stream
+/// We use AES with a 256-bit key and CTR/CFB cipher block mode, which gives us a stream
 /// cipher that can support arbitrary-length ciphertexts. The IV is used as an input to
 /// the cipher as the "block to supply before the first block of plaintext". This is
 /// required because all ciphers (except the weak ECB) are built such that each block
@@ -56,11 +65,13 @@ class IntegrityHash {
 /// encryption.
 class EncryptionKey {
  public:
-  EncryptionKey() : initialized_(false) {}
+  EncryptionKey() : initialized_(false) {
+    mode_ = SSLeay() < OPENSSL_VERSION_1_0_1 ? AES_256_CFB : AES_256_CTR;
+  }
 
   /// Initialize a key for temporary use with randomly generated data. Reinitializes with
-  /// new random values if the key was already initialized. We use AES-CFB mode so key/IV
-  /// pairs should not be reused. This function automatically reseeds the RNG
+  /// new random values if the key was already initialized. We use AES-CTR/AES-CFB mode
+  /// so key/IV pairs should not be reused. This function automatically reseeds the RNG
   /// periodically, so callers do not need to do it.
   void InitializeRandom();
 
@@ -75,6 +86,11 @@ class EncryptionKey {
   /// otherwise the buffers must not overlap.
   Status Decrypt(const uint8_t* data, int64_t len, uint8_t* out) const WARN_UNUSED_RESULT;
 
+  /// Specify a cipher mode. Maybe in future we can provide a configuration
+  /// option for the end user who can choose a preferred mode(GCM, CTR, CFB...)
+  /// based on their software/hardware environment.
+  void setCipherMode(AES_CIPHER_MODE m) { mode_ = m; }
+
  private:
   /// Helper method that encrypts/decrypts if 'encrypt' is true/false respectively.
   /// A buffer of input data 'data' of length 'len' is encrypted/decrypted with this
@@ -88,11 +104,17 @@ class EncryptionKey {
   /// uninitialized keys.
   bool initialized_;
 
+  /// return a EVP_CIPHER according to cipher mode at runtime
+  const EVP_CIPHER* getCipher() const;
+
   /// An AES 256-bit key.
   uint8_t key_[32];
 
   /// An initialization vector to feed as the first block to AES.
   uint8_t iv_[AES_BLOCK_SIZE];
+
+  /// Cipher Mode
+  AES_CIPHER_MODE mode_;
 };
 }
 
